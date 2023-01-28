f0c172890d92f057478efe88bde11f111550776b
kelvin <tearteamoguy@gmail.com>
2023-01-29 05:51:01 +0700

    more models added

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 """
 
-Models for ORM operations
+Models for ORM operations and define database relationships
 
 """
 
@@ -8,6 +8,7 @@ from .database import Base
 from sqlalchemy import Boolean,Integer,String,ForeignKey,Column
 from sqlalchemy.orm import relationship
 
+#User
 class User(Base):
     __tablename__= 'users'
 
@@ -21,15 +22,34 @@ class User(Base):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Instructors(Base):
+    __tablename__ = 'instructors'
+    instructor_id = Column(Integer, primary_key=True)
+    instructor_name = Column(String(20), nullable=False)
+    instructor_email = Column(String(20), nullable=False)
+    instructor_phone = Column(String(20), nullable=False)
+    course_available = Column(String(20), nullable=False)
+    course_id = Column(Integer, ForeignKey('courses.course_id'), nullable=False)
+
         
-class Post(Base):
-    __tablename__= 'posts'
+class Students(Base):
+    __tablename__ ='students'
+    student_id = Column(Integer, primary_key=True)
+    student_name = Column(String(20), nullable=False)
+    student_email = Column(String(20), nullable=False)
+    student_phone = Column(String(20), nullable=False)
+    class_enrollment = Column(String(20), nullable=False)
+    course_id = Column(Integer, ForeignKey('courses.course_id'), nullable=False)
 
-    id = Column(Integer, primary_key=True)
 
-    user_id = Column(Integer, ForeignKey('users.id'))
 
-    title = Column(String(100), nullable=False)
-
-    
+class Course(Base):
+    __tablename__ = 'courses'
+    course_id = Column(Integer, primary_key=True)
+    course_name = Column(String(20), nullable=False)
+    course_code = Column(String(20), nullable=False)
+    course_description = Column(String(200), nullable=False)
+    instructor_id = Column(Integer, ForeignKey('instructors.instructor_id'))
+    students = relationship('Students', backref='courses')
 
